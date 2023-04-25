be10280cdad91c1b3cbf6d9e683c1d282ad58739
Abderrahmane01Rhiabi <rhiabi.abderrahmane@gmail.com>
2023-04-25 22:11:03 +0200

    chained route

--- app.js
+++ app.js
@@ -7,73 +7,74 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
 mongoose.set({});
-mongoose.connect("mongodb://127.0.0.1:27017/wikiDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://127.0.0.1:27017/wikiDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("Connected to MongoDB successfully");
+db.once('open', function () {
+    console.log("Connected to MongoDB successfully");
 });
 
 const articlesSchema = {
-    title : String,
-    content : String
+    title: String,
+    content: String
 }
 
 const Article = mongoose.model("Article", articlesSchema);
 
-app.get("/articles", (req,res) =>{
+app.route("/articles")
 
-    Article.find({}).then((results) => {
-        // console.log(res);
-        res.send(results);
-    }).catch((err) => {
-        console.log(err);
+    .get((req, res) => {
+
+        Article.find({}).then((results) => {
+            console.log(res);
+            res.send(results);
+        }).catch((err) => {
+            console.log(err);
+        })
+
     })
 
-})
+    .put((req, res) => {
 
-app.post("/articles", (req,res) =>{
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        })
 
+        console.log(req.body.title);
+        console.log(req.body.content);
 
-    const newArticle = new Article({
-        title : req.body.title,
-        content : req.body.content
+        newArticle.save().then((result) => {
+            console.log(result);
+            res.send("added successfullty " + result);
+        }).catch((err) => {
+            console.log(err);
+        })
+
     })
 
-    console.log(req.body.title);
-    console.log(req.body.content);
-    
-    newArticle.save().then((result) => {
-        console.log(result);
-        res.send("added successfullty " + result);
-    }).catch((err) => {
-         console.log(err);
-    })
+    .delete((req, res) => {
 
-})
+        Article.deleteMany({}).then((result) => {
+            console.log(result);
+            res.send("deleted successfullty " + result);
+        }).catch((err) => {
+            console.log(err);
+        })
 
-app.delete("/articles",(req,res) => {
+    });
 
-    Article.deleteMany({}).then((result) => {
-        console.log(result);
-        res.send("deleted successfullty " + result);
-
-    }).catch((err) => {
-        console.log(err);
-    })
-
-})
 
 let port = process.env.PORT;
 
-if(port == null || port == "") port = 3000;
+if (port == null || port == "") port = 3000;
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log("Server has started Succusfully");
 })
